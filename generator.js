edfbc1c068621cd023c5fbacc70e3bed9ba37bc4
Ubuntu <nextor@gmail.com>
2024-10-31 20:24:46 +0000

    Adding index.php

--- generator.js
+++ generator.js
@@ -1,15 +1,16 @@
-window.addEventListener('load', initCodes );
+window.addEventListener('load', initCodes ); 
 window.addEventListener('resize', initCodes );
 
 
 function initCodes()
 {
 	let index = 0;
-	for(let c of window.grap_codes)
-	{
-		console.log(c);
+	/*
+		JsBarcode( '.jsbarcode').init();
+	*/
 
-		let id = '#p_'+index;
-		var barcode = new JsBarcode( id, c.value, c );
-	}
+	window.graph_codes.codes.forEach((c,index)=>
+	{
+		JsBarcode('#p_'+index,c.value,c);
+	});
 }
--- index.php
+++ index.php
@@ -1,15 +1,34 @@
 <?php
 namespace APP;
 
-include_once( __DIR__.'/app.php' );
+#include_once( __DIR__.'/app.php' );
 include_once( __DIR__.'/akou/src/ArrayUtils.php');
 include_once( __DIR__.'/SuperRest.php');
 
 use akou\ArrayUtils;
 
+$l = new SuperRest();
+$codes = $l->getMethodParams();
+
 
 $l = new SuperRest();
 $codes = $l->getMethodParams();
+$attributes = [];
+$error = '';
+foreach($codes['codes'] as $index=>$code)
+{
+
+	$attr_values = "";
+	foreach($code as $key=>$value)
+	{
+		$attr_values .= ' jsbarcode-'.$key.'="'.urlencode($value).'"';
+	}
+	$attributes[] = $attr_values;
+}
+
+
+
+$version = 30;
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,18 +37,20 @@ $codes = $l->getMethodParams();
 	<title>Generador de codigos</title>
 	<link rel="stylesheet" href="style.css" crossorigin="anonymous">
 	<script src="JsBarcode.all.min.js"></script>
-	<script src="generator.js"></script>
+	<script src="generator.js?version<?=$version?>"></script>
 	<script>
-		var grap_codes = <?=json_encode($codes, JSON_PRETTY_PRINT)?>;
+		var graph_codes = <?=json_encode($codes, JSON_PRETTY_PRINT)?>;
+
+		console.log(graph_codes);
 	</script>
 </head>
 <body>
 <?php if($error):?>
 	<div>Error: <?=$error?></div>
 <?php else:?>
-	<?php foreach($codes as $index=>$code):?>
+	<?php foreach($attributes as $i=>$attrs):?>
 	<div class="page">
-		<canvas id="p_<?=$index?>" width="400" height="200"></canvas>
+	<canvas id="p_<?=$i?>" class="jsbarcode" <?=$attrs?>  width="400" height="200"></canvas>
 	</div>
 	<?php endforeach;?>
 <?php endif;?>
